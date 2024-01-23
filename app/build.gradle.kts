e2f50109981d6bc6114179cb8d915df7eb471038
PAUL\jesus <jesusmrin29@gmail.com>
2024-01-22 19:55:33 -0700

    Ultimo hecho el viernes 19 en clase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.arima.practica1"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.arima.practica1"
         minSdk = 28
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
